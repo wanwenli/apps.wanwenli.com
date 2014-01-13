b430ed3091140c1fb69b47c7faf7a0d4dcc87d80
swwl1992 <swwl1992@gmail.com>
2014-01-13 20:01:26 +0800

    [bug fixed] invalid URL

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
-
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-    res.render('index', { title: 'Express' })
+    res.render('index', { title: 'Apps by Wan Wenli' })
 };
 
 exports.tycrawler = function(req, res){
-    res.render('tycrawler', {title: 'Crawler'});
+    res.render('tycrawler', {title: 'Tianya Crawler'});
 };
 
 exports.tycrawler_result = function(req, res){
@@ -18,6 +17,24 @@ exports.tycrawler_result = function(req, res){
     , async = require('async')
     , format = require('util').format;
 
+    // check the validity of URL
+    var url_validator = function(url){
+        if(url.indexOf('http://') == -1) {
+            res.end('Your URL must contain "http://"');
+            return false;
+        }
+        else if(url.indexOf('tianya.cn') == -1) {
+            res.end('Your URL is not from tianya.');
+            return false;
+        }
+        else if(url.indexOf('post') == -1) {
+            res.end('Your URL is not a post.');
+            return false;
+        }
+        else
+            return true;
+    }
+
     var init_url = req.body.url,
         delimiter = '|',
         pages = [],
@@ -25,6 +42,7 @@ exports.tycrawler_result = function(req, res){
         concurrency = 1;
 
     console.log(init_url);
+    is_valid_url = url_validator(init_url);
 
     // debugging purpose
     var log_comments_info = function (c_i) {
@@ -39,93 +57,99 @@ exports.tycrawler_result = function(req, res){
 
     // parse comments info into a string 
     var comments_info_tostring = function(ci) {
-        var result = '\nFloor';
-        for(var i = 0; i < ci.length; i++) {
-            result = result + (delimiter + ci[i][0]);
+        var result = '\nFloor' + delimiter + 'Content' + delimiter +
+            'Author' + delimiter + 'Device' + delimiter + 'Time';
+        for (var i = 0; i < ci.length; i++){
+            result = result + '\n';
+            for (var j = 0; j < 5; j++){
+                result = result + ci[i][j] + delimiter;
+            }
         }
-        var result = result + '\nContent';
-        for(var i = 0; i < ci.length; i++) {
-            result = result + (delimiter + ci[i][1]);
-        }
-        var result = result + '\nAuthor';
-        for(var i = 0; i < ci.length; i++) {
-            result = result + (delimiter + ci[i][2]);
-        }
-        var result = result + '\nDevice';
-        for(var i = 0; i < ci.length; i++) {
-            result = result + (delimiter + ci[i][3]);
-        }
-        var result = result + '\nTime';
-        for(var i = 0; i < ci.length; i++) {
-            result = result + (delimiter + ci[i][4]);
-        }
-
         return result;
     };
-
+    
     // crawl from each page stored in pages[]
-    request(init_url, function(e, re, bd) {
-        var $$ = cheerio.load(bd);
-        var last_page_num = parseInt($$(".atl-pages form a:nth-last-child(4)").html());
-
-        // get the page title
-        var page_title = $$("#post_head span.s_title span").html();
-        // get the author of the main post
-        var main_author = $$("#post_head .atl-info a").html();
-        console.log(page_title);
-        console.log(main_author);
-
-        for(var i = 1; i <= last_page_num; i++){
-            var url = init_url.substring(0, 37) + i + '.shtml';
-            console.log(url);
-            pages.push(url);
-        }
-
-        async.eachLimit(pages, concurrency, function (page, next) {
-            request(page, function (err, response, body) {
-                if (err) throw err;
-                var $ = cheerio.load(body);
-
-                // get content of each post on the page
-                var first_post = parseInt($("div.atl-item").first().attr('id'));
-                // if first_post does not exist
-                if (!first_post){
-                    first_post = 1;
+    if (is_valid_url) {
+        try {
+            request(init_url, function(e, re, bd) {
+                if (e) {
+                    res.end('Your URL ' + init_url + ' is not valid.');
+                    throw e;
                 }
-                var last_post = parseInt($("div.atl-item").last().attr('id'));
-                for(var id = first_post; id <= last_post; id++) {
-                    var content = $("#"+id+" .bbs-content").
-                        text().trim();
-                    var author = $("#"+id+" .atl-head .atl-info").
-                        children('span').eq(0).children().eq(0).text();
-                    var device;
-                    // in case device is empty
-                    if(device == null){
-                        device = 'N.A.';
-                    }
-                    var raw_time = $("#"+id+" .atl-head .atl-info").
-                        children('span:last-child').text();
-                    var time = raw_time.substr(3);
-                    comments_info.push([
-                        id,
-                        content,
-                        author,
-                        device,
-                        time
-                    ]);
+                var $$ = cheerio.load(bd);
+                var last_page_num = parseInt($$(".atl-pages form a:nth-last-child(4)").
+                    html());
+
+                // get the page title
+                var page_title = $$("#post_head span.s_title span").html();
+                // get the author of the main post
+                var main_author = $$("#post_head .atl-info a").html();
+                // get the content of the main post
+                var main_post = $$(".bbs-content.clearfix").html().trim();
+                console.log(page_title);
+                console.log(main_author);
+                console.log(main_post);
+
+                for(var i = 1; i <= last_page_num; i++){
+                    var url = init_url.substring(0, 37) + i + '.shtml';
+                    console.log(url);
+                    pages.push(url);
                 }
-                var page_num = pages.indexOf(page) + 1;
-                console.log('Page ' + page_num + ' complete');
-                if (page_num == last_page_num){
-                    var str = 'Page title' + delimiter + page_title;
-                    str = str + '\nMain author' + delimiter + main_author;
-                    str = str + comments_info_tostring(comments_info);
-                    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-                    res.end(str);
-                    console.log('Complete');
-                }
-                next();
+
+                async.eachLimit(pages, concurrency, function (page, next) {
+                    request(page, function (err, response, body) {
+                        if (err) {
+                            throw err;
+                            res.end('Server error');
+                        }
+                        var $ = cheerio.load(body);
+
+                        // get content of each post on the page
+                        var first_post = parseInt($("div.atl-item").first().attr('id'));
+                        // if first_post does not exist
+                        if (!first_post){
+                            first_post = 1;
+                        }
+                        var last_post = parseInt($("div.atl-item").last().attr('id'));
+                        for(var id = first_post; id <= last_post; id++) {
+                            var content = $("#"+id+" .bbs-content").
+                                text().trim();
+                            var author = $("#"+id+" .atl-head .atl-info").
+                                children('span').eq(0).children().eq(0).text();
+                            var device;
+                            // in case device is empty
+                            if(device == null){
+                                device = 'N.A.';
+                            }
+                            var raw_time = $("#"+id+" .atl-head .atl-info").
+                                children('span:last-child').text();
+                            var time = raw_time.substr(3);
+                            comments_info.push([
+                                id,
+                                content,
+                                author,
+                                device,
+                                time
+                            ]);
+                        }
+                        var page_num = pages.indexOf(page) + 1;
+                        console.log('Page ' + page_num + ' complete');
+
+                        if (page_num == last_page_num){
+                            var str = 'Page title' + delimiter + page_title;
+                            str = str + '\nMain author' + delimiter + main_author;
+                            str = str + '\nMain post' + delimiter + main_post;
+                            str = str + comments_info_tostring(comments_info);
+                            res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+                            res.end(str);
+                            console.log('Complete');
+                        }
+                        next();
+                    });
+                });
             });
-        });
-    });
+        } catch (err) {
+            console.log(err);
+        }
+    }
 };

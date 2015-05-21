b85b311521b4a4803f131cb07a9de4255be20037
swwl1992 <swwl1992@gmail.com>
2015-05-21 16:29:59 +0800

    fix bug and simplify return JSON

--- routes/zhihu.js
+++ routes/zhihu.js
@@ -2,7 +2,7 @@ var crawler = require('../crawl/zhihuapi.js');
 
 exports.userapi = function (req, res) {
 	var uid = req.params.uid;
-	crawler.user('endurance', function(user) {
+	crawler.user(uid, function(user) {
         res.json(user);
 	});
 };

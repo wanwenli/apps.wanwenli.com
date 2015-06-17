29bf9925614a20cf35e0312e23eda67dcd304078
swwl1992 <wwl.990@hotmail.com>
2015-06-17 10:44:16 +0800

    attempt to fix response bug but failed

--- crawl/zhihuapi.js
+++ crawl/zhihuapi.js
@@ -17,29 +17,31 @@ ZhihuApi.prototype.user = function(uid, jsCbName, callback) {
 	var userNotFoundObj = this.USER_NOT_FOUND_OBJ;
 	
 	this.request(url, function(err, response, body) {
-		var $ = cheerio.load(body);
-		var resObj = {};
-		if ($('div.error').length === 0) {
-			var id = $('a.zm-profile-header-user-detail').attr('href').split('/')[2];
-			resObj =  {
-				id : id,
-				url : baseUrl + peopleUrl + id,
-				avatar_url : $('div.zm-profile-header-avatar-container img').attr('src'),
-				name : $('div.title-section span.name').html(),
-				followees : parseInt($('div.zm-profile-side-following a:nth-child(1) strong').html(), 10),
-				followers : parseInt($('div.zm-profile-side-following a:nth-child(2) strong').html(), 10),
-				agree : parseInt($('span.zm-profile-header-user-agree strong').html(), 10),
-				thanks : parseInt($('span.zm-profile-header-user-thanks strong').html(), 10)
-			};
-		} else {
-			// 404 not found
-			resObj = userNotFoundObj;
+		if (!err && response.statusCode == 200) {
+			var $ = cheerio.load(body);
+			var resObj = {};
+			if ($('div.error').length === 0) {
+				var id = $('a.zm-profile-header-user-detail').attr('href').split('/')[2];
+				resObj =  {
+					id : id,
+					url : baseUrl + peopleUrl + id,
+					avatar_url : $('div.zm-profile-header-avatar-container img').attr('src'),
+					name : $('div.title-section span.name').html(),
+					followees : parseInt($('div.zm-profile-side-following a:nth-child(1) strong').html(), 10),
+					followers : parseInt($('div.zm-profile-side-following a:nth-child(2) strong').html(), 10),
+					agree : parseInt($('span.zm-profile-header-user-agree strong').html(), 10),
+					thanks : parseInt($('span.zm-profile-header-user-thanks strong').html(), 10)
+				};
+			} else {
+				// 404 not found
+				resObj = userNotFoundObj;
+			}
+			var resStr = JSON.stringify(resObj);
+			if (typeof jsCbName != 'undefined') {
+				resStr = jsCbName + '(' + resStr + ')';
+			}
+			callback(resStr);
 		}
-		var resStr = JSON.stringify(resObj);
-		if (typeof jsCbName != 'undefined') {
-			resStr = jsCbName + '(' + resStr + ')';
-		}
-		callback(resStr);
 	});
 };
 
